7bd359235ea7efea10859530e621986896c5d3e1
ouoio3-code <ouoio3@gmail.com>
2026-09-14 15:49:08 +0900

    import table
    import table

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'forest-pwa-v5-2d-latlon';
+const CACHE = 'forest-pwa-v6-csv-import';
 const FILES = ['./', './index.html', './manifest.json', './vol_table.js', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
