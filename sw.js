a311fc46f595f4109b4452424c4a911cf1bbb911
ouoio3-code <ouoio3@gmail.com>
2026-09-15 11:28:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'forest-pwa-v8-tree-copy';
+const CACHE = 'forest-pwa-v9-compare-to-survey';
 const FILES = ['./', './index.html', './manifest.json', './vol_table.js', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
